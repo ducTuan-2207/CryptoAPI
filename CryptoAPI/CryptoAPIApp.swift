88aa890ea5da3e450aa4d14db00ec4b113e3ee56
DucTuan <tuanhypc02@gmail.com>
2024-05-01 23:01:46 +0700

    Initial Commit

---
+++ CryptoAPI/CryptoAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAPIApp.swift
+//  CryptoAPI
+//
+//  Created by macOS on 01/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
