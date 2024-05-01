5610d4c810b7ab81a17d9b142f782baf96e4a1e0
DucTuan <tuanhypc02@gmail.com>
2024-05-02 01:04:25 +0700

    setup theme color

--- CryptoAPI/ContentView.swift
+++ CryptoAPI/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            VStack {
+                Text("Accent color")
+                    .foregroundColor(Color.theme.accent)
+                Text("Secondar color")
+                    .foregroundColor(Color.theme.secondaryText)
+                Text("Red color")
+                    .foregroundColor(Color.theme.red)
+                Text("Green color")
+                    .foregroundColor(Color.theme.green)
+            }
         }
-        .padding()
     }
 }
 
