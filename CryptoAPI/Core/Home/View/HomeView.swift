d053f5c6854c1bbd37e8021799f4e229663d9cba
DucTuan <tuanhypc02@gmail.com>
2024-05-05 00:32:43 +0700

    components

--- CryptoAPI/Core/Home/View/HomeView.swift
+++ CryptoAPI/Core/Home/View/HomeView.swift
@@ -26,6 +26,9 @@ struct HomeView: View {
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
+        NavigationView {
+            HomeView()
+                .navigationBarHidden(true)
+        }
     }
 }
