95701bf35e452e6c385a817a2fe69c6761d2beab
DucTuan <tuanhypc02@gmail.com>
2024-05-08 23:54:31 +0700

    coin row view

--- CryptoAPI/Core/Home/View/CoinRowView.swift
+++ CryptoAPI/Core/Home/View/CoinRowView.swift
@@ -8,15 +8,38 @@
 import SwiftUI
 
 struct CoinRowView: View {
-    
+    let coin: CoinModel
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 0){
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundColor(Color.theme.secondaryText)
+                .frame(minWidth: 30)
+            Circle()
+                .frame(width: 30, height: 30)
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .padding(.leading, 6)
+                .foregroundColor(Color.theme.accent)
+            Spacer()
+            VStack {
+                Text("\(coin.currentPrice)")
+                    .bold()
+                    .foregroundColor(Color.theme.accent)
+                Text("\(coin.priceChangePercentage24H ?? 0)")
+                    .foregroundColor(
+                        (coin.priceChange24H ?? 0 ) >= 0 ?
+                        Color.theme.green :
+                            Color.theme.red
+                    )
+            }
+        }
     }
 }
 
 struct CoinRowView_Previews: PreviewProvider {
     static var previews: some View {
-        CoinRowView()
+        CoinRowView(coin: dev.coin)
     }
 }
