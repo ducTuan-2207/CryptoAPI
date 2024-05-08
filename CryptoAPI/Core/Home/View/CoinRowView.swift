435eab6700c09786c2c357f7c1ec5acdd2e0fa72
DucTuan <tuanhypc02@gmail.com>
2024-05-08 23:25:01 +0700

    preview provider

---
+++ CryptoAPI/Core/Home/View/CoinRowView.swift
@@ -0,0 +1,22 @@
+//
+//  CoinRowView.swift
+//  CryptoAPI
+//
+//  Created by macOS on 08/05/2024.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CoinRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinRowView()
+    }
+}
