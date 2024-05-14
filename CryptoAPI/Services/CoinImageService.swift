90a9fadba6bc5a9d3e3b1dad79641fbaa9a72de8
DucTuan <tuanhypc02@gmail.com>
2024-05-14 23:02:59 +0700

    saving images done

--- CryptoAPI/Services/CoinImageService.swift
+++ CryptoAPI/Services/CoinImageService.swift
@@ -14,13 +14,26 @@ class CoinImageService {
     @Published var image: UIImage? = nil
     private var imageSuncription: AnyCancellable?
     private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_image"
+    private let imageName: String
     
     init(coin: CoinModel){
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
         
     }
     private func getCoinImage(){
+        if let saveImage = fileManager.getImage(imageName: coin.id, folderName: folderName) {
+            image = saveImage
+            print("Retrieved image from File Manager!")
+        } else {
+            downloadCoinImage()
+            print("Downloading image now!")
+        }
+    }
+    private func downloadCoinImage(){
         print("Downloading image now")
         guard let url = URL(string: coin.image) else {return}
         
@@ -33,8 +46,10 @@ class CoinImageService {
             .sink(receiveCompletion: NetwokingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
                 // Xử lý giá trị nhận được từ publisher
                 // Gán giá trị nhận được cho thuộc tính allCoins của đối tượng self
-                self?.image = returnedImage
-                self?.imageSuncription?.cancel()
+                guard let self = self, let downloadImage = returnedImage else {return}
+                self.image = downloadImage
+                self.imageSuncription?.cancel()
+                self.fileManager.saveImage(image: downloadImage, imageName: self.imageName, folderName: self.folderName)
             })
     }
     
