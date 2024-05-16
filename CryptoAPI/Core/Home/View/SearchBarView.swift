0417456a7db45212ba959df6d30a04d100009a92
DucTuan <tuanhypc02@gmail.com>
2024-05-16 16:21:23 +0700

    custom searchBar

---
+++ CryptoAPI/Core/Home/View/SearchBarView.swift
@@ -0,0 +1,56 @@
+//
+//  SearchBarView.swift
+//  CryptoAPI
+//
+//  Created by macOS on 16/05/2024.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    
+    @State var searchText = ""
+    
+    var body: some View {
+        HStack{
+            Image(systemName: "magnifyingglass")
+                .foregroundColor(
+                    searchText.isEmpty ? Color.theme.secondaryText : Color.theme.accent
+                )
+            TextField("Search by name or symbol ...", text: $searchText)
+                .foregroundColor(Color.theme.accent)
+                .overlay(
+                    Image(systemName: "xmark.circle.fill")
+                        .padding()
+                        .offset(x: 10)
+                        .foregroundColor(Color.theme.accent)
+                        .opacity(searchText.isEmpty ? 0.0 : 1.0)
+                        .onTapGesture {
+                            searchText = ""
+                        }
+                    ,
+                    alignment: .trailing
+                )
+        }
+        .font(.headline)
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 25)
+                .fill(Color.theme.background)
+                .shadow(color: Color.theme.secondaryText.opacity(15),
+                        radius: 5,x: 0, y: 0)
+        )
+        .padding()
+    }
+}
+
+struct SearchBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            SearchBarView()
+                .preferredColorScheme(.dark)
+            SearchBarView()
+                .preferredColorScheme(.light)
+        }
+    }
+}
