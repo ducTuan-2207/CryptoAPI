57519fa473d5e43ab57b4f9f03cd33d13cb3253e
DucTuan <tuanhypc02@gmail.com>
2024-05-21 17:09:06 +0700

    statistic view

--- CryptoAPI/Core/Models/StatisticModel.swift
+++ CryptoAPI/Core/Models/StatisticModel.swift
@@ -13,4 +13,13 @@ struct StatisticModel: Identifiable {
     let value: String
     let percentageChange: Double?
     
+    init(title: String, value: String, percentageChange: Double? = nil) {
+        self.title = title
+        self.value = value
+        self.percentageChange = percentageChange
+    }
+    
+    
 }
+
+//let newModel = StatisticModel(title: "", value: "", percentageChange: nil)
--- CryptoAPI/Extensions/PreviewProvider.swift
+++ CryptoAPI/Extensions/PreviewProvider.swift
@@ -23,6 +23,11 @@ class DeveloperPreview {
     
     let homeVM = HomeViewModel()
     
+    let state1 = StatisticModel(title: "Market Cap", value: "$123.5Bn", percentageChange: 22.07)
+    let state2 = StatisticModel(title: "Total volumn", value: "$223.5Tr")
+    let state3 = StatisticModel(title: "Portfolio Value", value: "$632.5K", percentageChange: -22.02)
+    
+    
 //    let stat1 = StatisticModel(title: "Market Cap", value: "$12.5Bn", percentageChange: 25.34)
 //    let stat2 = StatisticModel(title: "Total Volume", value: "$1.23Tr")
 //    let stat3 = StatisticModel(title: "Portfolio Value", value: "$50.4k", percentageChange: -12.34)
